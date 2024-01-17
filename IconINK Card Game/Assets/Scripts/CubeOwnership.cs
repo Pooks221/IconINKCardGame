5417a0da693ff184da33430a35bf0c704d62f250
Zefider <ZefiderGaming@gmail.com>
2024-01-16 19:42:06 -0600

    On Hover Set authority
    LETSSSS GOOOOOO

--- IconINK Card Game/Assets/Scripts/CubeOwnership.cs
+++ IconINK Card Game/Assets/Scripts/CubeOwnership.cs
@@ -2,14 +2,16 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Interaction;
+using Fusion;
 using UnityEngine;
 
-public class CubeOwnership : MonoBehaviourPunCallbacks
+public class CubeOwnership : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -27,5 +29,20 @@ public class CubeOwnership : MonoBehaviourPunCallbacks
         //{
         //    view.TransferOwnership(requestingPlayer);
         //}
+    }
+
+    public void RequestOwnership()
+    {
+        if (!Object.HasStateAuthority)
+        {
+            Object.RequestStateAuthority();
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    public void RemoveOwnership()
+    {
+        Object.ReleaseStateAuthority();
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
     }
 }

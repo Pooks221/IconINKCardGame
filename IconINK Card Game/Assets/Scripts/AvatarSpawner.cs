04cf7b40ec8fcd8452b2338da3d1b32e3be119d2
xMegx <meg.iconink@gmail.com>
2024-01-18 00:14:02 -0600

    Avatar work
    Now there are 2 avatars per player - the one you can see, and the one others can see. However, while the one others can see travels with you, the position doesn't seem to be updated to others (boo)

--- IconINK Card Game/Assets/Scripts/AvatarSpawner.cs
+++ IconINK Card Game/Assets/Scripts/AvatarSpawner.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using System;
 using Fusion.Sockets;
+using Oculus.Platform;
+using Oculus.Avatar2;
+using System.Threading.Tasks;
 
 public class AvatarSpawner : MonoBehaviour
 {
@@ -12,6 +16,8 @@ public class AvatarSpawner : MonoBehaviour
     public GameObject prefabAvatar;
     private NetworkObject go;
     private NetworkObject go2;
+
+    [SerializeField] Transform spawnerlocation;
 
     // Start is called before the first frame update
     //Comment to test git
@@ -23,34 +29,39 @@ public class AvatarSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(go2 != null)
-        {
-            Vector3 newPosition = Vector3.Lerp(transform.position, go.transform.position, 5f * Time.deltaTime);
-            go2.transform.position = newPosition;
-        }
+        //if(go2 != null)
+        //{
+        //    Vector3 newPosition = Vector3.Lerp(transform.position, go.transform.position, 5f * Time.deltaTime);
+        //    go2.transform.position = newPosition;
+        //}
     }
 
-    public void spawnAvatar()
+    public async void spawnAvatar()
     {
-       if(runner == null)
+        
+        if (runner == null)
         {
             runner = Fusion.NetworkRunner.GetRunnerForGameObject(gameObject);
         }
-        NetworkObject go = runner.Spawn(prefabAvatar, transform.position, transform.rotation);
-        go.transform.parent = gameObject.transform;
-        NetworkObject go2 = runner.Spawn(prefabAvatar, transform.position, transform.rotation);
-        Debug.Log("go" + go);
-        Debug.Log("go2" + go2);
+        NetworkObject go = runner.Spawn(prefabAvatar, transform.position, transform.rotation, runner.LocalPlayer);
+        go.transform.SetParent(spawnerlocation);
+        //go.transform.parent = gameObject.transform;
+        await Task.Delay(10000);
+        //Vector3 newPosition = new Vector3(0, 0, 0);
+        //go.transform.position = newPosition;
+        //go.transform.SetParent(spawnerlocation);
+        Debug.Log(go);
+        
 
     }
 
     public void reposition()
     {
-        Debug.Log("reposition one" + go2);
-        Debug.Log("reposition one" + go);
-        Vector3 newPosition = Vector3.Lerp(transform.position, go.transform.position, 5f * Time.deltaTime);
+        //Debug.Log("reposition one" + go2);
+        //Debug.Log("reposition one" + go);
+        Vector3 newPosition = new Vector3 (0, 0, 0);
         
-        go2.transform.position = newPosition;
+        spawnerlocation.transform.position = newPosition;
         //reposition();
     }
 
